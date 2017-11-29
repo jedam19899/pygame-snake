aac9b05288458ca3ad60b86fb69a8b8616c1c9f6
jedam19899 <34068980+jedam19899@users.noreply.github.com>
2017-11-29 11:53:12 -0500

    Update snake.py
    changed snake body and head

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 '''
 Snake game.
-Authors: Jesse Dampare, 
+Authors:
 MOBIUS SNAKE
 '''
 
@@ -30,9 +30,9 @@ DIRECTION_DOWN = (0, 1)
 # Background color of the snake grid.
 COLOR_BACKGROUND = (255, 255, 255)  # rgb color for white
 # This is the color of the snake's head. 
-COLOR_SNAKE_HEAD = (255, 0, 0)      # rgb color for red
+COLOR_SNAKE_HEAD = (0, 0, 0)      # rgb color for red
 # This is the color of the rest of the snake.
-COLOR_SNAKE = (0, 255, 0)           # rgb color for green
+COLOR_SNAKE = (250, 233, 36)           # rgb color for green
 # This is the color for the snake's food.
 COLOR_FOOD = (255, 200, 0)          # rgb color for orange
 # This is the color for the game over text.
@@ -48,14 +48,18 @@ def get_direction(previous_direction, event_key):
         return DIRECTION_LEFT
     elif event_key == pygame.K_UP:
         return DIRECTION_UP
+    elif event_key == pygame.K_DOWN:
+        return DIRECTION_DOWN
+    elif event_key == pygame.K_RIGHT:
+        return DIRECTION_RIGHT
     return previous_direction
 
 def create_food_position():
     """Returns a random 2-tuple in the grid where the food should be located.
     The first element is the x position. Must be an int between 0 and GRID_WIDTH - 1, inclusively.
     The second element is the y position. Must be an int between 0 and GRID_HEIGHT - 1, inclusively.
-    """
-    return
+    """   
+    return 
 
 def snake_ate_food(snake, food):
     """Returns whether food was eaten by the snake.
@@ -257,4 +261,3 @@ def start_game():
 
 # Start the snake game.
 start_game()
-#Add group member and changed group name

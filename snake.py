656ec2948cd9bac46485cb423a6402e1691e6ffd
jedam19899 <34068980+jedam19899@users.noreply.github.com>
2017-12-11 15:22:51 -0500

    Snake speed from ten to 5

--- snake.py
+++ snake.py
@@ -28,15 +28,15 @@ DIRECTION_UP = (0, -1)
 DIRECTION_DOWN = (0, 1)
 
 # Background color of the snake grid.
-COLOR_BACKGROUND = (25, 55, 3)  # rgb color for white
+COLOR_BACKGROUND = (255, 255, 255)  # rgb color for white
 # This is the color of the snake's head. 
-COLOR_SNAKE_HEAD = (29, 83, 51)      # rgb color for red
+COLOR_SNAKE_HEAD = (0, 0, 0)      # rgb color for red
 # This is the color of the rest of the snake.
-COLOR_SNAKE = (215, 223, 6)           # rgb color for green
+COLOR_SNAKE = (250, 233, 36)           # rgb color for green
 # This is the color for the snake's food.
-COLOR_FOOD = (55, 0, 6)          # rgb color for orange
+COLOR_FOOD = (255, 200, 0)          # rgb color for orange
 # This is the color for the game over text.
-COLOR_GAME_OVER_TEXT = (0, 99, 0)    # rgb color for black
+COLOR_GAME_OVER_TEXT = (0, 0, 0)    # rgb color for black
 
 def get_direction(previous_direction, event_key):
     """Return the new direction of the snake: one of DIRECTION_{LEFT,RIGHT,UP,DOWN}.
@@ -115,10 +115,10 @@ def get_game_over_text(score):
 def get_snake_speed(snake):
     """Return the number of cells the snake should travel in one second.
     snake - list of 2-tuples representing the positions of each snake segment
-    The speed at the beginning of the game should be 5. Once the snake has eaten 10 pieces of food,
+    The speed at the beginning of the game should be 5. Once the snake has eaten 10 pieces of food,  # THIS IS SUPPOSED TO BE 5 PIECES OF FOOD
     the speed of the game should increase (by how much is up to you).
     """
-    if len(snake) >= 20:
+    if len(snake) >= 15:
         return 10
     return 5
 
